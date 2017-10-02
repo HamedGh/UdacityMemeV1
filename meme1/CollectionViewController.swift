784724b81605f3f67482986ebf8e0794c4ea29db
Hamed.Gh <h.ghadirian@outlook.com>
2017-10-02 12:13:03 +0330

    1-Add collection view and table view controllers 2- some minor UI changes 3-add flow layout codes

---
+++ meme1/CollectionViewController.swift
@@ -0,0 +1,120 @@
+//
+//  CollectionViewController.swift
+//  meme1
+//
+//  Created by Hamed.Gh on 10/2/17.
+//  Copyright © 2017 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    @IBOutlet var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var memes : [Meme]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    func subscribeToOrientationNotification() -> Void {
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        NotificationCenter.default.addObserver(self, selector: #selector(deviceDidRotate(_:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    @objc func deviceDidRotate(_ notification: NSNotification) -> Void {
+        
+        switch UIDevice.current.orientation {
+        case .landscapeRight:
+            setupFlowLayout(orientation: .landscapeRight)
+        case .landscapeLeft:
+            setupFlowLayout(orientation: .landscapeLeft)
+        default:
+            setupFlowLayout(orientation: .portrait)
+        }
+    }
+    
+    func setupFlowLayout(orientation: UIDeviceOrientation) -> Void {
+        let space: CGFloat = 3.0
+        let dimension: CGFloat
+        
+        if orientation == .portrait {
+            dimension = (view.frame.size.width - (2 * space)) / 3.0
+        } else if orientation == .portraitUpsideDown {
+            dimension = (view.frame.size.width - (2 * space)) / 3.0
+        } else {
+            dimension = (view.frame.size.height - (2 * space)) / 3.0
+        }
+        
+        collectionViewFlowLayout.minimumInteritemSpacing = space
+        collectionViewFlowLayout.minimumLineSpacing = space
+        collectionViewFlowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+
+}
