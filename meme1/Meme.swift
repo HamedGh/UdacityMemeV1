f1abbd7fbefd2e28a85654a5c4b804eb3a8ed113
Hamed.Gh <h.ghadirian@outlook.com>
2017-10-02 09:46:39 +0330

    1-Bugfix icons, 2-Add struct to save meme, 3-Change UI in storyboard

---
+++ meme1/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  meme1
+//
+//  Created by Hamed.Gh on 10/2/17.
+//  Copyright © 2017 Hamed.Gh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topCaption: String
+    var bottomCaption: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+    
+}
